664cd2d9bd09dcfad35174df3bad733a9d78f32d
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2024-08-20 16:57:35 +0530

    header

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const mysql = require('mysql2');
 const multer = require('multer');
 
